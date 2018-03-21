853f9af98a2e56c1bd854881a2ff31670b39bfd6
lsh1990 <xtlsh1990@126.com>
2018-03-22 07:03:21 +0800

    hashcode与equal

---
+++ src/test/java/com/hashcode/Test.java
@@ -0,0 +1,24 @@
+package com.hashcode;
+
+/**
+ * @Description: hashCode与equal比较
+ * @author-lsh
+ * @date 2018年3月21日 下午8:22:00
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		Company one = new Company("张三", 18);
+		Company two = new Company("李四", 20);
+		Company three = new Company();
+		three.setName("张三");
+		three.setAge(18);
+		
+		System.out.println(one.equals(three) ? true : false);
+		System.out.println(one.equals(two) ? true : false);
+		System.out.println(one.hashCode());
+		System.out.println(three.hashCode());
+	}
+	
+	
+}
