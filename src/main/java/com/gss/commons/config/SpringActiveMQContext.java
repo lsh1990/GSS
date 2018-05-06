efc0d274482ed4711e6da7d9b7a82a225ae7706c
lsh1990 <xtlsh1990@126.com>
2018-05-06 22:01:49 +0800

    active

---
+++ src/main/java/com/gss/commons/config/SpringActiveMQContext.java
@@ -0,0 +1,36 @@
+package com.gss.commons.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.gss.uitls.Constant.DataTransmitter;
+import com.gss.uitls.TransmitterPropertyConfigs.CommonConfig;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Description: activemq 动态配置项
+ * @author-lsh
+ * @date 2018年5月4日 下午2:56:24
+ */
+@Configuration
+@Slf4j
+public class SpringActiveMQContext {
+	
+	/**
+	 * @Description: 配置JMS连接工厂， 引入spring的mq连接池。可以配置缓存的连接数 100
+	 * @return 
+	 */
+	@Bean
+	public ActiveMQConnectionFactory activeMqConnectionFactory() {
+		if (StringUtils.equalsIgnoreCase(CommonConfig.TRANS_SRC, DataTransmitter.ACTIVEMQ)) {
+			ActiveMQConnectionFactory mq = new ActiveMQConnectionFactory();
+			mq.setBrokerURL("");
+		}
+		
+		return null;
+	}
+
+}
---
+++ src/main/java/com/gss/uitls/TransmitterPropertyConfigs.java
@@ -0,0 +1,51 @@
+package com.gss.uitls;
+
+/**
+ * @Description: 传输器配置
+ * @author-lsh
+ * @date 2018年5月3日 下午4:57:47
+ */
+public class TransmitterPropertyConfigs {
+	
+	/**
+	 * @Description: 公用配置
+	 * @author-lsh
+	 * @date 2018年5月3日 下午5:03:35
+	 */
+	public static class CommonConfig {
+		
+		public static final String TRANS_SRC = Constant.TRANSPORT_BUNDLE.getString("trans.src", "");
+		
+	}
+	
+	/**
+	 * @Description: activemq 方式传输配置
+	 * @author-lsh
+	 * @date 2018年5月4日 下午4:56:34
+	 */
+	public static class ActiveMQConfig {
+		//数据接收队列
+    	public static final String RECEIVE_QUEUE = Constant.TRANSPORT_BUNDLE.getString("activemq.edi.receive.queue");
+
+    	//数据接收队列
+    	public static final String RECEIPT_QUEUE = Constant.TRANSPORT_BUNDLE.getString("activemq.edi.receipt.queue");
+    	
+    	//activemq中文件名
+    	public static final String FILENAME_PROPERTY = Constant.TRANSPORT_BUNDLE.getString("activemq.edi.filename.property");
+    	
+    	//用户名
+    	public static final String USERNAME = Constant.TRANSPORT_BUNDLE.getString("activemq.edi.username");
+    	
+    	//密码
+    	public static final String PASSWORD = Constant.TRANSPORT_BUNDLE.getString("activemq.edi.password");
+    	
+    	//密码
+    	public static final String PORT = Constant.TRANSPORT_BUNDLE.getString("activemq.edi.port", "61616");
+    	
+    	//brokerURL
+    	public static final String BROKER_URL = Constant.TRANSPORT_BUNDLE.getString("activemq.edi.brokerurl");
+	}
+	
+	
+
+}
